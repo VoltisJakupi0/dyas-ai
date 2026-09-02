3395a2bc5e13e63779daa24a2ea7bc40c4e536e0
Voltis Jakupi <voltis.jakupi@spaceiq.com>
2026-09-02 16:42:26 +0200

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,6 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#eeece7" },
-    { media: "(prefers-color-scheme: dark)", color: "#eeece7" },
-  ],
 };
 
 export const metadata: Metadata = {
